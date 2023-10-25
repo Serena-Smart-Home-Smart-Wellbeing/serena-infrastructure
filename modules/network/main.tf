9d5c52aa3031b05259f19c2d7bdc291ff9fb469e
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-10-25 21:36:45 +0700

    Add private google access to asia subnet

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -4,10 +4,11 @@ resource "google_compute_network" "serena-net" {
 }
 
 resource "google_compute_subnetwork" "serena-subnet-asia-southeast2" {
-  name          = "serena-subnet-asia-southeast2"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = var.GCP_REGION
-  network       = google_compute_network.serena-net.self_link
+  name                     = "serena-subnet-asia-southeast2"
+  ip_cidr_range            = "10.0.0.0/24"
+  region                   = var.GCP_REGION
+  network                  = google_compute_network.serena-net.self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_firewall" "serena-net-allow-ssh" {
