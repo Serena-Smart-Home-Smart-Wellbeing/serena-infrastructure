c83853b50e3b1ba69621e79df25ca4dbade79a4c
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-10-25 09:16:49 +0700

    Use cloud-storage module

--- main.tf
+++ main.tf
@@ -2,10 +2,16 @@ module "services" {
   source = "./modules/services"
 }
 
-module "iam-tim-serena-project-viewer" {
+module "iam" {
+  depends_on     = [module.services]
   source         = "./modules/iam"
   GCP_PROJECT_ID = var.GCP_PROJECT_ID
 
+}
+
+module "cloud-storage" {
   depends_on = [module.services]
+  source     = "./modules/cloud-storage"
+  GCP_REGION = var.GCP_REGION
 }
 
