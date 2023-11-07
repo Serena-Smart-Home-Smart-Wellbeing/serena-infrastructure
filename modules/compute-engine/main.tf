bedd24ac2e99da8fbd1a33f4df35dd282b09994c
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-07 16:17:45 +0700

    Remove image-uploader
    We can upload through Vertex AI Workbench

--- modules/compute-engine/main.tf
+++ modules/compute-engine/main.tf
@@ -1,39 +1,39 @@
-resource "google_service_account" "image-uploader" {
-  account_id   = "image-uploader"
-  display_name = "image-uploader"
-  description  = "SA for image uploader"
-}
+# resource "google_service_account" "image-uploader" {
+#   account_id   = "image-uploader"
+#   display_name = "image-uploader"
+#   description  = "SA for image uploader"
+# }
 
-resource "google_service_account_iam_binding" "image-uploader-admins" {
-  service_account_id = google_service_account.image-uploader.name
-  role               = "roles/iam.serviceAccountAdmin"
+# resource "google_service_account_iam_binding" "image-uploader-admins" {
+#   service_account_id = google_service_account.image-uploader.name
+#   role               = "roles/iam.serviceAccountAdmin"
 
-  members = [
-    "user:c200bsy3485@bangkit.academy",
-    "user:mreyhanapwsw@gmail.com"
-  ]
-}
+#   members = [
+#     "user:c200bsy3485@bangkit.academy",
+#     "user:mreyhanapwsw@gmail.com"
+#   ]
+# }
 
-resource "google_compute_instance" "image-uploader" {
-  name         = "image-uploader"
-  machine_type = "e2-standard-2"
+# resource "google_compute_instance" "image-uploader" {
+#   name         = "image-uploader"
+#   machine_type = "e2-standard-2"
 
-  boot_disk {
-    initialize_params {
-      size  = 10
-      type  = "pd-standard"
-      image = "debian-cloud/debian-11"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       size  = 10
+#       type  = "pd-standard"
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
 
-  network_interface {
-    subnetwork = var.subnetwork
-  }
+#   network_interface {
+#     subnetwork = var.subnetwork
+#   }
 
-  allow_stopping_for_update = true
+#   allow_stopping_for_update = true
 
-  service_account {
-    scopes = ["cloud-platform"]
-    email  = google_service_account.image-uploader.email
-  }
-}
+#   service_account {
+#     scopes = ["cloud-platform"]
+#     email  = google_service_account.image-uploader.email
+#   }
+# }
