7ae07113c2e4f14b2a73b69505375624358f018b
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-07 19:37:55 +0700

    Fix location from region to zone

--- modules/vertex-ai/main.tf
+++ modules/vertex-ai/main.tf
@@ -1,6 +1,6 @@
 resource "google_notebooks_instance" "emotion-detector-nb" {
   name            = "emotion-detector-nb"
-  location        = "asia-southeast2"
+  location        = "asia-southeast2-a"
   machine_type    = "n1-standard-4"
   service_account = var.emotion-detector-nb-sa-email
 
