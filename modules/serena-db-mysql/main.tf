0e5e153606c28ccff000878e5f0e42ac722ce66b
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-23 11:57:27 +0700

    Downgrade sql instance

--- modules/serena-db-mysql/main.tf
+++ modules/serena-db-mysql/main.tf
@@ -6,7 +6,7 @@ resource "google_sql_database_instance" "serena-main" {
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
-    tier              = "db-n1-standard-1"
+    tier              = "db-f1-micro"
     availability_type = "REGIONAL"
     backup_configuration {
       enabled            = true
