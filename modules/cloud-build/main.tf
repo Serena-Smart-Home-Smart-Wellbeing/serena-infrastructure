20e13a36ba3b9de6e3c0b9d991e4085f6c2cd621
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-24 16:57:54 +0700

    Change cloud build sa email and import substitutions

--- modules/cloud-build/main.tf
+++ modules/cloud-build/main.tf
@@ -3,7 +3,7 @@ resource "google_cloudbuild_trigger" "serena-backend-github-trigger" {
   location    = "global"
   description = "Build and deploy to Cloud Run service serena-backend on push to \"^main$\""
 
-  # https://github.com/Serena-Smart-Home-Smart-Wellbeing/serena-infrastructure
+  # https= github.com/Serena-Smart-Home-Smart-Wellbeing/serena-infrastructure
   github {
     owner = "Serena-Smart-Home-Smart-Wellbeing"
     name  = "serena-backend"
@@ -21,4 +21,12 @@ resource "google_cloudbuild_trigger" "serena-backend-github-trigger" {
     "gcp-cloud-build-deploy-cloud-run-managed",
     "serena-backend",
   ]
+  substitutions = {
+    _AR_HOSTNAME   = "asia-southeast2-docker.pkg.dev"
+    _DEPLOY_REGION = "asia-southeast2"
+    _ENTRYPOINT    = "npm run start"
+    _PLATFORM      = "managed"
+    _SERVICE_NAME  = "serena-backend"
+    _TRIGGER_ID    = "d980f73f-fa5c-4dbd-8af6-ee9e67854f79"
+  }
 }
