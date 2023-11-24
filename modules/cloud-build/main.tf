1ba72b200d870f09c77ca14bf1a3136ecd654045
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-11-24 16:53:12 +0700

    Remove `enterprise_config_resource_name `

--- modules/cloud-build/main.tf
+++ modules/cloud-build/main.tf
@@ -1,7 +1,7 @@
 resource "google_cloudbuild_trigger" "serena-backend-github-trigger" {
   name        = "serena-backend-github-trigger"
   location    = "global"
-  description = "Build and deploy to Cloud Run service serena-backend on push to ^main$"
+  description = "Build and deploy to Cloud Run service serena-backend on push to \"^main$\""
 
   # https://github.com/Serena-Smart-Home-Smart-Wellbeing/serena-infrastructure
   github {
@@ -10,7 +10,6 @@ resource "google_cloudbuild_trigger" "serena-backend-github-trigger" {
     push {
       branch = "^main$"
     }
-    enterprise_config_resource_name = "projects/123456789/locations/us-central1/githubEnterpriseConfigs/configID"
   }
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
 
