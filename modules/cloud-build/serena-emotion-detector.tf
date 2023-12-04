047abb01965bde94e88f362e6f9b9ad759237b3d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-04 23:16:51 +0700

    Create `serena-emotion-detector-github-trigger`

---
+++ modules/cloud-build/serena-emotion-detector.tf
@@ -0,0 +1,18 @@
+resource "google_cloudbuild_trigger" "serena-emotion-detector-github-trigger" {
+  name        = "serena-emotion-detector-github-trigger"
+  location    = "global"
+  description = "Build and deploy to Cloud Run service serena-emotion-detector on push to \"^main$\""
+
+  github {
+    owner = "Serena-Smart-Home-Smart-Wellbeing"
+    name  = "serena-emotion-detector"
+    push {
+      branch = "^main$"
+    }
+  }
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+
+  service_account = var.serena-cloud-build-sa-id
+
+  filename = "/deploy/cloudbuild.yaml"
+}
--- modules/cloud-run/variables.tf
+++ modules/cloud-run/variables.tf
@@ -4,7 +4,7 @@ variable "location" {
 
 variable "serena-backend-image" {
   type    = string
-  default = "asia-southeast2-docker.pkg.dev/serena-777/cloud-run-source-deploy/serena-backend/serena-backend:01751c83c6a310cd897d0de7a6787e2930765f53"
+  default = "asia-southeast2-docker.pkg.dev/serena-777/cloud-run-source-deploy/serena-backend/serena-backend"
 }
 
 variable "serena-backend-sa-email" {
