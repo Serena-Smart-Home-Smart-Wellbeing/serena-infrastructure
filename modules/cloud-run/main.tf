e102d8c7eb1a7fb6acfb1a3914fb9a6dabac639f
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-04 23:27:42 +0700

    Remove serena-backend cloud run
    WIll be managed through cloudbuild.yaml for its trigger instead

--- modules/cloud-run/main.tf
+++ modules/cloud-run/main.tf
@@ -1,46 +1,46 @@
-resource "google_cloud_run_service" "serena-backend" {
-  name     = "serena-backend"
-  location = var.location
+# resource "google_cloud_run_service" "serena-backend" {
+#   name     = "serena-backend"
+#   location = var.location
 
-  template {
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale" = "1",
-        "autoscaling.knative.dev/minScale" = "0",
-      }
-    }
-    spec {
-      containers {
-        image = var.serena-backend-image
+#   template {
+#     metadata {
+#       annotations = {
+#         "autoscaling.knative.dev/maxScale" = "1",
+#         "autoscaling.knative.dev/minScale" = "0",
+#       }
+#     }
+#     spec {
+#       containers {
+#         image = var.serena-backend-image
 
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
+#         resources {
+#           limits = {
+#             cpu    = "1000m"
+#             memory = "512Mi"
+#           }
+#         }
 
-        ports {
-          container_port = 8080
-        }
-      }
-      service_account_name = var.serena-backend-sa-email
-    }
-  }
+#         ports {
+#           container_port = 8080
+#         }
+#       }
+#       service_account_name = var.serena-backend-sa-email
+#     }
+#   }
 
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
 
-resource "google_cloud_run_service_iam_binding" "serena-backend-invoker" {
-  location = var.location
-  service  = google_cloud_run_service.serena-backend.name
-  role     = "roles/run.invoker"
+# resource "google_cloud_run_service_iam_binding" "serena-backend-invoker" {
+#   location = var.location
+#   service  = google_cloud_run_service.serena-backend.name
+#   role     = "roles/run.invoker"
 
-  members = [
-    "allUsers"
-  ]
-}
+#   members = [
+#     "allUsers"
+#   ]
+# }
