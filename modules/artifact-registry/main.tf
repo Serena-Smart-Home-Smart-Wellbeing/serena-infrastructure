0f460c26e357ae08211669b69e825058a64a42c5
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-16 19:51:25 +0700

    Add KEEP 3 cleanup policy to `cloud-run-source-deploy`

--- modules/artifact-registry/main.tf
+++ modules/artifact-registry/main.tf
@@ -1,9 +1,19 @@
 resource "google_artifact_registry_repository" "cloud-run-source-deploy" {
+  provider      = google-beta
   location      = var.location
   repository_id = "cloud-run-source-deploy"
   description   = "Cloud Run Source Deployments"
   format        = "DOCKER"
   mode          = "STANDARD_REPOSITORY"
+
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 3
+    }
+  }
 }
 
 resource "google_artifact_registry_repository_iam_binding" "cloud-run-source-deploy-admins" {
